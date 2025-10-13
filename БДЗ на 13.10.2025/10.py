f78a223fb6df9d837a4bedbf296b8b36c7ad0681
Gur-Sych <dany.gurchin@gmail.com>
2025-10-13 11:27:05 +0300

    Up to 13.10.2025

--- БДЗ на 13.10.2025/10.py
+++ БДЗ на 13.10.2025/10.py
@@ -1,5 +1,17 @@
+import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = sp.Symbol('x')
+y = 1/(np.exp(x**2-1))
+x_r = 1
+a1 = sp.limit(y,x,x_r,'-')
+a2 = sp.limit(y,x,x_r,'+')
+print(f"Является устранимой точкой разрыва при {x_r}:","да" if a1==a2 else "нет")
+x_r = -1
+a1 = sp.limit(y,x,x_r,'-')
+a2 = sp.limit(y,x,x_r,'+')
+print(f"Является устранимой точкой разрыва при {x_r}:","да" if a1==a2 else "нет")
 
 x = np.linspace(-10,10,1000)
 y = 1/(np.exp(x**2-1))
--- БДЗ на 13.10.2025/7.py
+++ БДЗ на 13.10.2025/7.py
@@ -1,7 +1,7 @@
 import sympy as sp
 
 x = sp.Symbol('x')
-f = (x**2-1)*sp.ln(x)
+f = ((x**2)-1)*sp.ln(x)
 a = sp.limit(f/x**-.00000000000000000000000000000000000000000000000000000001,x,0)
 print(a)
 
@@ -9,7 +9,7 @@ def range(start,end,step):
     while(start < end):
         start += step; yield start
 
-for r in range(-.2,.2,.01):
+for r in range(-.0002,.0002,.00001):
     a = sp.limit(f/x**r,x,0)
-    if a not in (-sp.oo, 0, sp.oo):
-        print(f'{a}, r = {r}')
+    #if a not in (-sp.oo, 0, sp.oo):
+    print(f'{a}, r = {r}')
